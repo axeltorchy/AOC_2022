4f6bde212f3243d6ea6af26ae70ad36a660f80ae
Axel Torchy <axel.torchy@gmail.com>
2022-12-09 10:43:13 +0100

    Add alternative solution for day 3 part 1

--- code/day3.py
+++ code/day3.py
@@ -5,10 +5,13 @@ inputfile = utils.INPUT_DIR / "day3.txt"
 
 # Set priorities
 priorities = {}
+reverse_priorities = {}
 for index, letter in enumerate(string.ascii_lowercase):
    priorities[letter] = index + 1
+   reverse_priorities[index + 1] = letter
 for index, letter in enumerate(string.ascii_uppercase):
    priorities[letter] = index + 27
+   reverse_priorities[index + 1] = letter
 
 with open(inputfile, "r") as fh:
     lines = fh.readlines()
@@ -25,8 +28,29 @@ for line in lines:
             total_priorities += priorities[item]
             break
 
-print(f"Total priorities: {total_priorities}")
+print(f"(Part 1 - method 1) Total priorities: {total_priorities}")
 
+# Alternative part 1
+total_priorities = 0
+for line in lines:
+    line = line.strip()
+    nb_items = len(line)
+    first_half, second_half = line[:nb_items//2], line[nb_items//2:]
+
+    items_first_half, items_second_half = 0, 0
+    for char in first_half:
+        items_first_half |= 1 << priorities[char]
+    for char in second_half:
+        items_second_half |= 1 << priorities[char]
+    
+    common_item = items_first_half & items_second_half
+    for i in range(len(priorities)):
+        letter_pos = 1 << (i+1)
+        if common_item == letter_pos:
+            total_priorities += i+1
+            break
+
+print(f"(Part 1 - method 2) Total priorities: {total_priorities}")
 
 # Part 2
 total_priorities_badges = 0
@@ -48,7 +72,7 @@ for i in range(len(lines)//3):
 
             if letters[char] == 7:
                 total_priorities_badges += priorities[char]
-                print(f"Found badge: {char}, priority {priorities[char]}")
+                #print(f"Found badge: {char}, priority {priorities[char]}")
                 break
 
 
