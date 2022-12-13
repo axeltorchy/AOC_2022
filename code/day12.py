933dc8598312199516c147e6423c09b79913db3e
Axel Torchy <axel.torchy@gmail.com>
2022-12-13 11:14:54 +0100

    WIP day 12 part 2

--- code/day12.py
+++ code/day12.py
@@ -22,6 +22,7 @@ starting_point = None
 destination_point = None
 x = 0
 
+elevation_a = []
 # Initialize graph
 neighbors = {}
 for i in range(N_lines):
@@ -36,6 +37,8 @@ for i in range(N_lines):
             destination_point = (i, j)
 
         value = letters.get(letter)
+        if value == 1:
+            elevation_a.append((i,j))
         reachable = []
         if i > 0 and letters.get(lines[i-1][j]) <= value + 1:
             reachable.append((i-1, j))
@@ -48,17 +51,45 @@ for i in range(N_lines):
 
         neighbors[(i, j)] = reachable
 
+class UpdatablePriorityQueue():
+    def __init__(self):
+        self.nodes = []
+    
+    def put(self, item):
+        # Check if exists, update
+        updated = False
+        for i in range(len(self.nodes)):
+            if self.nodes[i][1] == item[1]:
+                self.nodes[i] = item
+        # otherwise add to queue
+        if not updated:
+            self.nodes.append(item)
+        self.nodes.sort()
+        self.nodes.reverse()
+
+    
+    def exists(self, item):
+        return item in self.nodes
+
+    def get(self):
+        return self.nodes.pop()
+
+    def empty(self):
+        return len(self.nodes) == 0
+
 def shortest_path(neighbors, start, dest):
     ### DIJKSTRA SPF ALGORITHM
     visited = set()
     # Insert starting point with distance 0
-    queue = PriorityQueue()
+    queue = UpdatablePriorityQueue()
     queue.put((0, start))
+    print(f"Added node {start}. Queue is: {queue.nodes}")
     min_distance = {}
     min_distance[start] = 0
 
     while not queue.empty():
         dist, node = queue.get()
+        print(f"Adding node {node} to visited.")
         if node == dest:
             break
         if node not in visited:
@@ -67,7 +98,23 @@ def shortest_path(neighbors, start, dest):
                 if successor not in min_distance or distance < min_distance[successor]:
                     min_distance[successor] = distance
                     queue.put((distance, successor))
+                    print(f"Added node {node}. Queue is: {queue.nodes}")
         visited.add(node)
+    if dest not in min_distance:
+        print("STOP")
+        print("Start:", start)
+        print("Destination:", dest)
+        print(min_distance)
     return min_distance[dest]
     
 print(shortest_path(neighbors, starting_point, destination_point))
+
+# Part 2
+min_dist = N_lines * N_columns
+for start in elevation_a:
+    dist = shortest_path(neighbors, start, destination_point)
+    if dist < min_dist:
+        min_dist = dist
+
+print(destination_point)
+print(f"Shortest path from any elevation 'a': {min_dist}")
